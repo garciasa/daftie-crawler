3eab4237feccd95b393d9a13d6a427e8256e60cb
garciasa <garciasa@gmail.com>
2020-02-23 17:29:26 +0000

    Adding frontend yml for github actions

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <section className="header flex items-center mx-auto bg-houseBlue-light h-12">
       <div className="flex-shrink-0 text-white font-semibold pl-2">
-        HouseCrawler <span className="text-xs">v0.1</span>
+        HouseCrawler <span className="text-xs">v0.2</span>
       </div>
       <div className="mx-auto text-white">Wexford Town</div>
     </section>
