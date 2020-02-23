c04495358a86fb2ade8a16825fa961fbc740d9ad
garciasa <garciasa@gmail.com>
2020-02-23 17:10:35 +0000

    forcing a change to see if github actions works

--- backend/postgres/postgres.go
+++ backend/postgres/postgres.go
@@ -1,9 +1,10 @@
 package postgres
 
 import (
+	"log"
+
 	"github.com/go-pg/pg/v9"
 	_ "github.com/lib/pq"
-	"log"
 )
 
 // New Postgres connection
@@ -11,7 +12,7 @@ func New(opts *pg.Options) *pg.DB {
 	db := pg.Connect(opts)
 
 	if db == nil {
-		log.Fatal("Error connecting to db")
+		log.Fatal("Error connecting to db...")
 	}
 
 	return db
