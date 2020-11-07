47f1963dea1311f8fc2714512695b231389eef3b
garciasa <garciasa@gmail.com>
2020-11-07 17:57:47 +0000

    Adding new endpoints

--- backend/handlers/stat.go
+++ backend/handlers/stat.go
@@ -1,10 +1,30 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+)
 
 func (s *Server) getStats() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		stats, err := s.domain.GetStats()
+		if err != nil {
+			badRequestResponse(w, err)
+		}
+
+		jsonResponse(w, stats, http.StatusOK)
+	}
+}
+
+func (s *Server) getStatsForCharts() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		beds, err := strconv.Atoi(r.URL.Query().Get("beds"))
+		if err != nil {
+			badRequestResponse(w, err)
+		}
+		
+		provider := r.URL.Query().Get("provider")
+		stats, err := s.domain.GetStatsForCharts(provider, beds)
 		if err != nil {
 			badRequestResponse(w, err)
 		}
